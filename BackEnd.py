111feed5ccfa5d4d5786346ecee2e5b100a20a27
pvalgaeren <r0781850@student.thomasmore.be>
2022-11-15 19:58:18 +0100

    update BackEnd.py

--- BackEnd.py
+++ BackEnd.py
@@ -13,15 +13,19 @@ async def get_menu():
     return {"menu": menu}
 
 @app.get("/dish")
-async def get_dish():
-    list = []
-    for key, value in menu:
-        if key == "ID":
-            list.append(value)
-    for n in list:
-        print(n)
-        number = random.randrange(len(list))
-        for k, v in menu:
-            if k == 'ID' and v == number:
-                if k == 'naam':
-                    return(v)
+def get_dish():
+    return(menu[random.choice(menu)]['naam'])
+
+        #print()
+        #for value in i:
+            #print(value)
+            #for j in value:
+                #print(j)
+                #if value == "ID":
+                    #list.append(j)
+    #for n in list:
+        #number = random.choice(list)
+        #for k, v in menu:
+            #if k == 'ID' and v == number:
+                #if k == 'naam':
+                   #return(v)
