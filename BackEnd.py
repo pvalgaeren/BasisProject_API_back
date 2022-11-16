b678b09dddd1e0e95a18e71335e40c8447ec967b
pvalgaeren <r0781850@student.thomasmore.be>
2022-11-16 11:13:26 +0100

    update BackEnd.py2

--- BackEnd.py
+++ BackEnd.py
@@ -1,6 +1,8 @@
+import random
 from fastapi import FastAPI, Query
 import json
 from pydantic import BaseModel
+
 
 app = FastAPI(debug=True)
 
@@ -22,21 +24,16 @@ with open("food.json") as file:
 async def get_menu():
     return {"menu": menu}
 
+# Een random gerecht uit het menu weergeven
+@app.get("/dish")
+async def get_dish():
+    return{"dish": random.choice(menu)}
 
-# Een gerecht uit de menukaart halen via het id
-@app.get("/dish/{number}", tags=["id"])
-async def get_dish(number: int | None = Query(
-    default=None,
-    description="Het id van het gerecht waar je meer informatie over wil.")):
-    if number in menu:
-        return menu[number]
-    else:
-        return "Het id dat u heb opgegeven hoort niet bij een gerecht"
 
 
 # Een gerecht toevoegen aan de menukaart
 @app.post("/new_dish", response_model=Dish, tags=["menu"])
 async def make_new_dish(dish: Dish):
-    key= max(menu, key=menu.get) + 1
+    key = max(menu, key=menu.get) + 1
     menu[key] = dish
     return menu[key]
