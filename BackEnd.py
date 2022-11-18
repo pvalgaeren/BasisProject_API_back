039d354dbb3951ffeff0e6a682e87d2ba8f8c0a9
pvalgaeren <r0781850@student.thomasmore.be>
2022-11-18 17:48:41 +0100

    update backend

--- BackEnd.py
+++ BackEnd.py
@@ -14,6 +14,7 @@ class Dish(BaseModel):
     id: int
 
 # Open json file
+menu = {}
 with open("food.json") as file:
   menu = json.load(file)
   print(menu)
@@ -22,15 +23,18 @@ with open("food.json") as file:
 # Het hele menu opvragen
 @app.get("/menu")
 async def get_menu():
-    return {"menu": menu}
+    return {"menu": menu["menu"]}
 
 # Een random gerecht uit het menu weergeven
 @app.get("/dish")
 async def get_dish():
-    return{"dish": random.choice(menu)}
+    return{"dish": random.choice(menu["menu"])}
 
 
 # Een gerecht toevoegen aan de menukaart
 @app.post("/new_dish", response_model=Dish)
 async def make_new_dish(dish: Dish):
+    menu["menu"].append(dish.dict())
+    with open("food.json", "w") as file:
+        json.dump(menu, file, indent=4)
     return dish
--- MakingJsonFile.py
+++ MakingJsonFile.py
@@ -1,30 +1,32 @@
 import json
 
 
-food = [
-    {
-        "Naam": "spaghetti bolognese",
-        "Categorie": "pasta",
-        "Allergie": "gluten, ei, selderij",
-        "Oorsprong": "Zuid-Italie",
-        "id": 1
-    },
-    {
-        "Naam": "sushi",
-        "Categorie": "rijst",
-        "Allergie": "gluten, eieren, schaaldieren, vis, pinda, soja, melk, noten",
-        "Oorsprong": "Zuidoost-Azie",
-        "id": 2
-    },
-    {
-        "Naam": "tiramisu",
-        "Categorie": "dessert",
-        "Allergie": "melk, gluten, ei",
-        "Oorsprong": "Italie",
-        "id": 3
-    },
+food = {
+    "menu": [
+        {
+            "Naam": "spaghetti bolognese",
+            "Categorie": "pasta",
+            "Allergie": "gluten, ei, selderij",
+            "Oorsprong": "Zuid-Italie",
+            "id": 1
+        },
+        {
+            "Naam": "sushi",
+            "Categorie": "rijst",
+            "Allergie": "gluten, eieren, schaaldieren, vis, pinda, soja, melk, noten",
+            "Oorsprong": "Zuidoost-Azie",
+            "id": 2
+        },
+        {
+            "Naam": "tiramisu",
+            "Categorie": "dessert",
+            "Allergie": "melk, gluten, ei",
+            "Oorsprong": "Italie",
+            "id": 3
+        }
 
-]
+    ]
+}
 
 json_object = json.dumps(food, indent=4)
 
